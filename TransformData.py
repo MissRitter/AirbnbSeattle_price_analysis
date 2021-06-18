884bccd6d21ca45bc402d4b6064985fc388dc2f9
Wiebke Ritter <wiebkeritter@freenet.de>
2021-06-18 20:08:54 +0200

    Sum: Detailed Look at listings data-set; preparation for data analysis
    Changes:
    
    * New notebook 01_explore_categorical_values.ipynb:
      * Questions for analysis posed
      * Exploration of listings.csv data-set
      * Categorical column-transformations in preparation for analysis
      * Some columns are dropped here in preparation for the analysis
    * New file TransformData.py:
      General auxiliary functions for data transformation

---
+++ TransformData.py
@@ -0,0 +1,79 @@
+
+import numpy as np
+import pandas as pd
+
+
+
+
+
+
+def date_transform(df, date_column, prefix=''):
+    '''
+    INPUT - 
+    df - the pandas dataframe with a datetime-type column
+    date_column - the datetime-type columns name
+
+    OUTPUT - 
+    df_trans - the original dataframe df with three etxra 
+    columns 'year', 'month' and 'day' coresponding to the date_column
+    '''
+    df_trans = df
+    # Transform the data calumns in df_trans to date-type
+    df_trans[date_column] = pd.to_datetime(df_trans[date_column])
+    # Split the date information into three columns
+    df_trans[prefix+"day"] = df_trans[date_column].map(lambda x: x.day)
+    df_trans[prefix+"month"] = df_trans[date_column].map(lambda x: x.month)
+    df_trans[prefix+"year"] = df_trans[date_column].map(lambda x: x.year)
+
+    return df_trans
+
+
+def price_transform(usd_col):
+    '''Returns the input column without $-sings and as float-type'''
+
+    col_return = usd_col
+    col_return = col_return.replace(
+        '[\$,]', '', regex=True).astype(float)
+    return col_return
+
+
+def rate_transform(percentage_col):
+    '''Returns the input column without %-sings and as float-type'''
+
+    col_return = percentage_col
+    col_return = col_return.replace(
+        '[\%,]', '', regex=True).astype(float)
+    return col_return
+
+
+def split_column_values(df, col_name, real_values, prefix=''):
+    '''
+    Returns the dataframe df with its original column col_name
+    containing strings of listed values.
+    In addition there will be new columns supplied by the
+    column name list real_values.
+    The new columns carry flags when the column name apears in the
+    respective col_name entry.
+
+    INPUT:
+    df - the pandas dataframe with col_name as a column
+    col_name - the column name you want to look through
+    real_values - a list of strings you want to search for 
+    in each row of df[col_name]
+
+    OUTPUT:
+    df - original df plus new columns with values {0,1}
+    '''
+
+    #loop through list of values
+    for val in real_values:
+        new_col_name = prefix+val
+        df[new_col_name] = 0
+        #loop through rows
+        for idx in df.index:
+            #if the pattern val is contained in this rows: set val to 1
+            if val in df.loc[idx, col_name]:
+                df.loc[idx, new_col_name] = 1
+    return df
+
+
